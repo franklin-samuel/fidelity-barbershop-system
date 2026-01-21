90b94bed8895fde83c3a8659246ca23db9013f07
Samuel Franklin <samuelfranklinff@gmail.com>
2026-01-20 21:16:30 -0300

    Create CorsConfiguration.java

---
+++ src/main/java/app/system/fidelity/security/configuration/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package app.system.fidelity.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final var configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
