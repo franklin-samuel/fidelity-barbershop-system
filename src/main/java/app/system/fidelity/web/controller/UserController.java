22752bc76ab13587cde74c25e49a000e2d44397b
Samuel Franklin <samuelfranklinff@gmail.com>
2026-02-22 02:38:16 -0300

    Update UserController.java

--- src/main/java/app/system/fidelity/web/controller/UserController.java
+++ src/main/java/app/system/fidelity/web/controller/UserController.java
@@ -55,7 +55,7 @@ public class UserController {
     @GetMapping
     public ResponseEntity<ApiResponse<List<UserResponse>>> list() {
 
-        final List<User> users = repository.findAllBarbers();
+        final List<User> users = repository.findAllAdmins();
 
         final List<UserResponse> responses = users.stream()
                 .map(mapper::mapToResponse)
