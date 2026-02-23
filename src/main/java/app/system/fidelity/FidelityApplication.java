d4ec949561e234d79379c371d1cbfa83c5747b20
Samuel Franklin <samuelfranklinff@gmail.com>
2026-02-23 12:33:24 -0300

    Update FidelityApplication.java

--- src/main/java/app/system/fidelity/FidelityApplication.java
+++ src/main/java/app/system/fidelity/FidelityApplication.java
@@ -1,10 +1,18 @@
 package app.system.fidelity;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FidelityApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FidelityApplication.class, args);
