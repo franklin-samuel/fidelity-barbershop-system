4f44f62d5b4e6f7490428dfee2b1557319a456ca
Samuel Franklin <samuelfranklinff@gmail.com>
2026-02-24 14:01:36 -0300

    Update AppointmentRepository.java

--- src/main/java/app/system/fidelity/persistence/repository/AppointmentRepository.java
+++ src/main/java/app/system/fidelity/persistence/repository/AppointmentRepository.java
@@ -3,11 +3,16 @@ package app.system.fidelity.persistence.repository;
 import app.system.fidelity.persistence.model.AppointmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<AppointmentEntity, UUID> {
     List<AppointmentEntity> findAllByOrderByCreatedAtDesc();
     List<AppointmentEntity> findByBarberIdOrderByCreatedAtDesc(UUID barberId);
+
+    List<AppointmentEntity> findByCreatedAtBetweenOrderByCreatedAtDesc(LocalDateTime start, LocalDateTime end);
+    List<AppointmentEntity> findByBarberIdAndCreatedAtBetweenOrderByCreatedAtDesc(UUID barberId, LocalDateTime start, LocalDateTime end);
+
     long countByLoyaltyDiscountApplied(boolean loyaltyDiscountApplied);
 }
