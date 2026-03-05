a9a597e6cb6fef7f3f94c628d9fe83832bb11cdc
Samuel Franklin <samuelfranklinff@gmail.com>
2026-03-05 08:14:23 -0300

    feat: trying set timezone straight, fuck!!!

---
+++ src/main/java/app/system/fidelity/persistence/configuration/DataSourceConfiguration.java
@@ -0,0 +1,16 @@
+package app.system.fidelity.persistence.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource dataSource() {
+        return new HikariDataSource();
+    }
+}
